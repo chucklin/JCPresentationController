b804fa230f0a4d8139ef2ccead0cee9db3f3a8e3
Chuck Lin <chuck42315@gmail.com>
2020-11-12 10:46:59 +0800

    Initial commit

---
+++ JCPresentationRemote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JCPresentationRemote
+//
+//  Created by chuck_lin on 2020/11/12.
+//  Copyright © 2020 Chuck Lin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JCPresentationRemote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JCPresentationRemote
+//
+//  Created by chuck_lin on 2020/11/12.
+//  Copyright © 2020 Chuck Lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
